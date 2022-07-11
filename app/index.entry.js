b5ef8c768b4d79afe9903bd19f1b480001ed53c9
irina.shasheva <irina.shasheva@jetbrains.com>
2022-07-11 11:58:48 +0300

    feat: add fallback for layout in ie11

--- app/index.entry.js
+++ app/index.entry.js
@@ -1,7 +1,13 @@
 import 'reset-css';
 import './exterrnal-libs/layout/index.css'
+import fallbackStylesheetUrl from '!!file-loader!./exterrnal-libs/layout/layout-without-css-vars.css';
+import { supportsCssVars, createLink } from './exterrnal-libs/layout/utils';
 import './index.entry.pcss';
-import "./exterrnal-libs/breakpoints/index.css";
+import './exterrnal-libs/breakpoints/index.css';
+
+if (!supportsCssVars()) {
+  createLink(fallbackStylesheetUrl);
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 
